06c0e6eefcdb28b84ed899b1903b139dc7ae359f
KevinBjarnemark <kevin.bjarnemark1@gmail.com>
2024-05-23 10:11:00 +0200

    Migrating to the 'sys system' by creating a new print_frame function + adjusted some 'slip-ups'

--- run.py
+++ run.py
@@ -10,6 +10,8 @@ character_index = 20
 current_character = "A"
 stickman_index = 7
 cycle = 0
+printed_frame = ["", "", "", "", ""]
+
 
 def print_frame_old():
     global stickman_index, character_index, current_character
@@ -40,34 +42,11 @@ def print_frame_old():
         print(line)
 
 def print_frame():
-    global stickman_index, character_index, current_character
-
-    # Draw the scene (except for bottom row)
-    printed_frame = [
-        "",
-        "",
-        "    O  ",
-        "   /|\\ ",
-        "",
-    ]
-
-    printed_frame[4] = ""
-    # Draw the scene
-    for i in range(0, STEPS):
-        if i == 3:
-            printed_frame[4] += "/" # Left leg 
-        elif i == 5:
-            printed_frame[4] += "\\" # Right leg 
-        elif i == character_index:
-            printed_frame[4] += current_character # Current character
-        else:
-            printed_frame[4] += "_" # Ground
-
-    # Print the drawn scene (frame)
-    for line in printed_frame:
-        print(line)
-
-
+    global printed_frame, stickman_index, character_index, current_character
+    
+    sys.stdout.write(f"\033[{5}A") # Move cursor up 5 lines
+    for i in range(0, len(printed_frame)):
+        sys.stdout.write(f"Row {i}\n")
 
 def choose_random_character():
     global current_character, character_index
@@ -75,27 +54,19 @@ def choose_random_character():
     character_index = STEPS # Reset 
 
 def start_game():
+    global printed_frame, character_index
     # Set color
     print(f"\x1B[38;2;{100};{169};{231}m") # RGB color
     # Draw empty lines to draw on 
-    for i in range(0, len(STEPS)):
+    for i in range(0, len(printed_frame)):
         print("\n")
-
-
 
     while True:
         # Choose a new character when it moves out of bounds 
         if character_index <= 0:
             choose_random_character()
 
-        # print_frame()
-
-        sys.stdout.write(f"\033[{5}A") # Move cursor up 5 lines/rows
-        sys.stdout.write("Row 1\n")
-        sys.stdout.write("Row 2\n")
-        sys.stdout.write("Row 3\n")
-        sys.stdout.write("Row 4\n")
-        sys.stdout.write("Row 5\n")
+        print_frame()
 
         time.sleep(speed)
         # Decrement the character_index
