9762a6e9052f035e1ef800ff0a7ee5e6446d7f11
KevinBjarnemark <kevin.bjarnemark1@gmail.com>
2024-05-27 19:45:22 +0200

    Removed the 'bottom-most' character as frames are printed to make room for more characters

--- run.py
+++ run.py
@@ -1,19 +1,11 @@
 import time
-import random
 import sys
-import threading
 
 character_list = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
 STEPS = 20
-JUMPING_RANGE = [8, 9, 10]
 speed = 0.3
-character_index = 20
-current_character = "A"
-stickman_index = 7
 cycle = 0
 printed_frame = ["", "", "", "", ""]
-jumping = False
-frames_jumped = 0
 frame_count = 0
 
 def print_frame():
@@ -39,6 +31,9 @@ def print_frame():
         sliced = printed_frame[y][:10] + character_list[y] + printed_frame[y][10:]
         printed_frame[y] = sliced
 
+    if frame_count >= rows and character_amount > 0:
+        character_list.pop(0)
+
     # Print the current frame 
     sys.stdout.write(f"\033[{len(printed_frame)}A") # Move cursor to the top
     for i in range(0, len(printed_frame)):
@@ -49,24 +44,8 @@ def print_frame():
     
     frame_count += 1
 
-def choose_random_character():
-    global current_character, character_index
-    current_character = random.choice(character_list)
-    character_index = STEPS # Reset 
-
-def get_input():
-    global jumping
-    while True:
-        user_input = sys.stdin.read(1)
-        if user_input == current_character:
-            jumping = True
-
 def start_game():
-    global printed_frame, character_index, STEPS
-    # Listen to the user input
-    input_thread = threading.Thread(target=get_input)
-    input_thread.daemon = True  # Allow the thread to exit when the main program exits
-    input_thread.start()
+    global printed_frame
     # Set color
     print(f"\x1B[38;2;{100};{169};{231}m") # RGB color
     # Create empty lines to draw on 
@@ -75,13 +54,7 @@ def start_game():
 
     # Game logic
     while True:
-        # Choose a new character when it moves out of bounds 
-        if character_index <= 0:
-            choose_random_character()
-
         print_frame()
         time.sleep(speed)
-        # Decrement the character_index
-        character_index -= 1
 
 start_game()
