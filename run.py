30dc7a0a883244013a402ecaa184f97ba357ae30
KevinBjarnemark <kevin.bjarnemark1@gmail.com>
2024-05-28 16:04:08 +0200

    Added docstrings, refactored code, reset edited variables, and refined the document troughout

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ import sys
 import random
 import copy
 
+# Character groups chosen based on the difficulty level
 character_bank = {
     "alphabet": [
         "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
@@ -30,7 +31,6 @@ difficulty = {
     "character_entries": [], # List of allowed entries in character_bank
 }
 speed = 0.7
-
 character_list = [] # List of dictionaries
 character_list_copy = [] # List of dictionaries
 printed_frame = ["", "", "", "", ""]
@@ -42,13 +42,16 @@ running = False
 character_inc = rows-1 # Increased for each 'cycle'
 
 # Helpers
-def count_down(num):
+def count_down(num, starting_in = False):
+    """Counts down in seconds"""
+    if starting_in:
+        print("Starting in...")
     for i in range(0, num):
         print(num - i)
         time.sleep(1)
 
-# Generate a random nuance of green
 def random_green_nuance():
+    """Generates a random nuance of green"""
     r = random.randint(40, 140)
     g = random.randint(200, 250)
     b = random.randint(80, 180)
@@ -67,23 +70,45 @@ def clear_canvas():
     printed_frame[3] = "   /|\\                "
     printed_frame[4] = "___/_\\________________"
 
-# Examine user answers
+
 def user_answer():
+    """Ask the user to submit their answer and examine if the 
+    answer is accepted"""
     global character_list_copy
     answer = str(input("Type in all characters loosely eg. ABCD123#% \n"))
     result = all(char["character"] in answer for char in character_list_copy)
 
     if result:
         print("Good work, you got it right! Starting next round...")
-        time.sleep(3)
+        time.sleep(3) # Give the user some time to read
         count_down(3)
     else:
         print("Oh no, one or more characters were incorrect..")
         input("Press enter to start over.\n")
     return result
 
+def check_user_results():
+    """Checks the user answer, resets edited variables and 
+    choses 'path' based on the user results"""
+    global character_inc, frame_count, running
+    if user_answer():
+        character_inc += 1 # Introduce more characters
+        frame_count = 0 # Reset frame_count
+        build_matrix_rain()
+        running = True # Run game
+    else:
+        # TODO Reset game
+        print("TODO, reset game")
+
 # Print the current frame
 def build_frame():
+    """Builds the current frame (matrix rain).
+    1. Sets the color
+    2. Clears the canvas
+    3. Calculates where to insert characters
+    4. Removes characters that are out of bounds
+    5. Increments the frame count 
+    6. Checks the user results when all characters are out of bounds"""
     global printed_frame, frame_count, character_list, rows, running, character_inc
     # Green color effect
     if frame_count % 5 == 0:
@@ -104,7 +129,7 @@ def build_frame():
         if frame_count < rows and character_amount > 0:
             y = frame_count-1 - i
 
-        # Insert character in based on x and y
+        # Insert character based on x and y
         sliced = printed_frame[y][:x] + character_list[i]["character"] + printed_frame[y][x:]
         printed_frame[y] = sliced
         
@@ -113,25 +138,12 @@ def build_frame():
         character_list.pop(0)
 
     frame_count += 1
-    
-    if character_amount <= 0:
-        running = False
-        if user_answer():
-            # Bring in more characters
-            character_inc += 1
-            # Reset values
-            character_list = []
-            frame_count = 0
-            # Build matrix rain
-            build_matrix_rain()
-            # Run game
-            running = True
-        else:
-            # TODO Reset game
-            print("TODO, reset game")
-        
 
-    time.sleep(speed) # Limit the 'prinitng speed'
+    # If all characters are out of bounds
+    if character_amount <= 0:
+        check_user_results()
+    else:
+        time.sleep(speed) # Limit the 'prinitng speed'
 
 def print_frame():
     """Executes the 'frame printing' after that frame has been built"""
@@ -144,21 +156,33 @@ def print_frame():
         sys.stdout.write(printed_frame[i] + "\n")
         sys.stdout.flush() # Flush immediately to ensure DOM rendering
 
-def game_setup():
+def user_input_welcome():
+    """Ask the user what to do and what settings to use"""
     global difficulty
-    # User interaction
+
     print("Welcome!") 
     # Choose what to do
     input_what_to_do = int(input("What do you want to do?\n 1. Play memorizing game\n"))
     print("Great!")
     # Choose difficulty 
     input_difficulty = int(input("How skilled are you at memorizing? \nType in a number between 1-10\n"))
-    
     # Set difficulty
     difficulty["level"] = input_difficulty
 
+def game_setup():
+    """Reset game settings and declare new settings"""
+    global difficulty
+
+    user_input_welcome()
+
+    # Reset edited variables
+    difficulty["level"] = 1
+    difficulty["character_entries"] = []
+    
     level = difficulty["level"]
     entries = difficulty["character_entries"]
+
+    # Add the entries to character groups based on difficulty level
     if level >= 1:
         entries.append("alphabet")
     if level >= 2:
@@ -173,10 +197,14 @@ def game_setup():
         entries.append("symbols_expert")
 
 def build_matrix_rain():
-    global printed_frame, character_bank, rows, difficulty, character_list, character_list_copy, character_inc
+    """Choose which characters that will be included in the matrix rain 
+    and append those to the character_list"""
+    global character_bank, rows, difficulty, character_list, character_list_copy, character_inc
+    # Reset previously edited variable
+    character_list = []
+    # Get the entries based on the difficulty level
     entries = difficulty["character_entries"]
-
-    # Note, this system doesn't support fewer characters than the amount of rows
+    # Choose random characters in random predetermined entries
     for i in range(0, max(character_inc, rows)): 
         random_entry = entries[random.randrange(len(entries))]
         character_bank_entry = character_bank[random_entry]
@@ -187,19 +215,17 @@ def build_matrix_rain():
 
 def start_game():
     global printed_frame, frame_count, running
+
     # Create empty lines to draw on 
     for i in range(0, len(printed_frame)):
         sys.stdout.write("\n")
 
     game_setup()
     build_matrix_rain()
-    # User prompt
-    print("Starting in...")
-    count_down(3)
+    count_down(3, True)
 
+    # Run game
     running = True
-
-    # Game logic
     while running:
         build_frame()
         print_frame()
