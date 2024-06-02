db7e1529e3f715d5e5ac24cf83fdd6c3a7a37495
KevinBjarnemark <kevin.bjarnemark1@gmail.com>
2024-06-02 22:09:00 +0200

    Added a file for storing static assets

---
+++ assets/python/static_assets.py
@@ -0,0 +1,21 @@
+"""Static assets"""
+
+
+GAME_EXPLANATION = (
+"""
+After completing this game setup random characters 
+will start to rain down the screen similar to the  
+movie 'The Matrix'.
+
+1. Try to remember all the characters in order. In 
+the first round you'll have to memorize 5 characters.
+
+2. Submit your answer.
+
+3. If you submit the correct answer you will be sent 
+to the next round.
+
+The number of characters will increase for each round.
+
+"""
+)
