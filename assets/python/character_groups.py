1d8672f0cdd8cf1504596c61a48df6ef2506a78a
KevinBjarnemark <kevin.bjarnemark1@gmail.com>
2024-06-05 06:21:21 +0200

    Changed the order of the list elements and added contextual comments

--- assets/python/character_groups.py
+++ assets/python/character_groups.py
@@ -1,21 +1,28 @@
 """A storage for different types of characters"""
+
+
 # Character groups chosen based on the difficulty level
+# NOTE Place the more difficult characters at the end. 
+# The algorithm will dig deeper into each group the higher 
+# the difficulty is. eg. l is very similar to 1
+
 CHARACTER_GROUPS = {
     "alphabet": [
-        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
-        "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "m", 
+        "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "y", "z", 
+        "q", "l",
     ],
     "numbers": [
         "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
     ],
     "symbols_easy": [
-        "!", "@", "#", "%", ",", ".", "?"
+        "?", ".", "!", "@", "#", "%", ","
     ],
     "symbols_intermediate": [
-        "$", "*", "(", ")", "_", "=", "+", "&", "/"
+        "/", "=", "+", "_", "$", "*", "(", ")", "&"
     ],
     "symbols_advanced": [
-        "-", "[", "]", ":"
+        ":", "-", "[", "]"
     ],
     "symbols_expert": [
         "^", "{", "}", ";", "'", "\"", "<", ">", "\\", "|", "~", "`"
